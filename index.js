13f9e9f195c95e341a11fc272b93a7fed91eae99
Jonathanbir <jonathan79111@gmail.com>
2023-01-27 12:50:18 +0800

    commit: signup 註冊帳號

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // 設定routes
 app.use("/auth", authRoutes);
